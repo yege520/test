8cb91e7039b8f9911d0862286f2204eed5677e93
yege520 <yege520@github.com>
2017-08-20 18:02:27 +0800

    second commit

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 # -*- coding:utf-8 -*-
-
+import os
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
