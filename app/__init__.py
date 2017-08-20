593977af69224cd0001ec27198e5840e7a0e0b81
yege520 <yege520@github.com>
2017-08-20 19:39:36 +0800

    add branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('config')
-db=SQLAlchemy(app)
+import os
+import app
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
-from app import views, models
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
